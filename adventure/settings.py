dce3a863142e652511c9b913db4f94328c584153
LeoKnox <LeoKnox@users.noreply.github.com>
2020-11-28 19:46:39 -0800

    Add files via upload

--- adventure/settings.py
+++ adventure/settings.py
@@ -50,9 +50,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheader.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'adventure.urls'
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 TEMPLATES = [
     {
