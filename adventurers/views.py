e3a9c3edcf10c491a2dabb1fe26ead5f520f0f6a
LeoKnox <LeoKnox@users.noreply.github.com>
2020-11-29 20:45:36 -0800

    Add files via upload

--- adventurers/views.py
+++ adventurers/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework.response import response
+from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
 
