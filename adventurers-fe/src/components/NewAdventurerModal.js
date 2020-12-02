b39d5d10bca35f11c4c69c5a1a90ef6963eab748
LeoKnox <LeoKnox@users.noreply.github.com>
2020-12-01 19:47:19 -0800

    Add files via upload

---
+++ adventurers-fe/src/components/NewAdventurerModal.js
@@ -0,0 +1,54 @@
+import NewAdventurerForm from "./NewStudentForm";
+
+class NewAdventurerModal extends Component {
+    state = {
+        modal: false
+    };
+
+    toggle = () => {
+        this.setState(previous => ({
+            modal: !previous.modal
+        }));
+    };
+
+    render() {
+        const create = this.props.create;
+
+        var title = "Editing Adventurer";
+        var button = <Button onclick={this.toggle}>Edit</Button>;
+
+        if (create) {
+            title = "Creating New Adventurer";
+
+            button = (
+                <Button
+                    color="primary"
+                    className="float-right"
+                    onClick={this.toggle}
+                    style={{ minWidth: "200px" }}
+                >
+                    Create New
+                </Button>
+            );
+        }
+
+        return (
+            <Fragment>
+                {button}
+                <Model isOpen={this.state.modal} toggle={this.toggle}>
+                    <ModalHeader toggle={this.toggle}>{title}</ModalHeader>
+
+                    <ModalBody>
+                        <NewAdventurerForm
+                            resetState={this.props.resetState}
+                            toggle={this.toggle}
+                            student={this.props.adventurer}
+                        />
+                    </ModalBody>
+                </Model>
+            </Fragment>
+        );
+    }
+}
+
+export default NewAdventurerModal;
