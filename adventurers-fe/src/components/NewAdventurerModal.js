2d59bff0ec2418229392ab2e0d987a5898e12bee
LeoKnox <LeoKnox@users.noreply.github.com>
2020-12-02 19:22:50 -0800

    Add files via upload

--- adventurers-fe/src/components/NewAdventurerModal.js
+++ adventurers-fe/src/components/NewAdventurerModal.js
@@ -1,3 +1,5 @@
+import React, { Component, Fragment } from "react";
+import { Button, Modal, ModalHeader, ModalBody } from "reactstrap";
 import NewAdventurerForm from "./NewStudentForm";
 
 class NewAdventurerModal extends Component {
