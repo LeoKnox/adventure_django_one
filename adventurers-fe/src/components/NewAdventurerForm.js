d2eeb6a7cd2ea377deb1a990247e56b42627e457
LeoKnox <LeoKnox@users.noreply.github.com>
2020-12-04 19:32:59 -0800

    Add files via upload

--- adventurers-fe/src/components/NewAdventurerForm.js
+++ adventurers-fe/src/components/NewAdventurerForm.js
@@ -1,11 +1,11 @@
-import React, {Component} from "react";
+import React from "react";
 import { Button, Form, FormGroup, Input, Label } from "reactstrap";
 
 import axios from "axios";
 
 import { API_URL } from "../constants";
 
-class NewAdventurerForm extends Component {
+class NewAdventurerForm extends React.Component {
     state = {
         pk: 0,
         name: "",
